2f987b9d27c287c7fd52ac30e0ba446ae6a6444f
Ravana <47528708+ravana69@users.noreply.github.com>
2019-03-28 11:41:40 +0530

    Update pmpermit.py

--- stdplugins/pmpermit.py
+++ stdplugins/pmpermit.py
@@ -14,7 +14,7 @@ borg.storage.PM_WARNS = {}
 BAALAJI_TG_USER_BOT = "My Master hasn't approved you to PM."
 TG_COMPANION_USER_BOT = "Please wait for his response and don't spam his PM."
 UNIBORG_USER_BOT_WARN_ZERO = "I am currently offline. Please do not SPAM me."
-UNIBORG_USER_BOT_NO_WARN = "Hi! Eye will answer to your message soon. Please wait for my response and don't spam my PM. Thanks"
+UNIBORG_USER_BOT_NO_WARN = "Hi! I will answer to your message soon. Please wait for my response and don't spam my PM. Thanks"
 
 
 @borg.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
