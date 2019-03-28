fda10e1cd18b5123043fdff6c92f8de880b00070
Ravana <47528708+ravana69@users.noreply.github.com>
2019-03-28 21:55:17 +0530

    Update call_admin.py

--- stdplugins/call_admin.py
+++ stdplugins/call_admin.py
@@ -4,11 +4,11 @@ from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
-@borg.on(events.NewMessage(pattern=r"\@a", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\@admin", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "@admin"
+    mentions = "🔴 Hey Admin, This Guy Is Spamming Here 🔴"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f"[\u2063](tg://user?id={x.id})"
