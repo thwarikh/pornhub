0d8ec3935b7ad39c1dfb7f31ca53841bbf720d4e
iamlordutkarsh <48874181+iamlordutkarsh@users.noreply.github.com>
2019-03-29 15:07:52 +0530

    Update and rename ravanareboot.py to utkarsh.py

--- stdplugins/utkarsh.py
+++ stdplugins/utkarsh.py
@@ -6,11 +6,11 @@ import asyncio
 from collections import deque
 
 
-@borg.on(events.NewMessage(pattern=r"\.ravana", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.utkarsh", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
-	deq = deque(list("RAVANA"))
+	deq = deque(list("UTKARSH"))
 	for _ in range(43):
 		await asyncio.sleep(0.1)
 		await event.edit("".join(deq))
